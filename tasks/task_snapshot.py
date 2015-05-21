38002b336b1aab948c04119b259f95cc173ef03f
Fabrizio Guglielmino <guglielmino@gmail.com>
2015-05-21 21:41:36 +0200

    Snapshot task update

--- tasks/task_snapshot.py
+++ tasks/task_snapshot.py
@@ -1,6 +1,8 @@
 import sys, os
 import time
 import io
+from PIL import Image
+
 
 try:
     from picamera.array import PiRGBArray
@@ -17,6 +19,7 @@ import settings
 
 from image_lib import overlay_image, fadein, create_empty_image
 from task_common import TaskFrameProcessorBase
+from image_lib import overlay_image, overlay_np_image_pi, overlay_pil_image_pi
 
 class SnapShotTask(TaskFrameProcessorBase):
 	'''
@@ -25,6 +28,7 @@ class SnapShotTask(TaskFrameProcessorBase):
 	STILL_FRAME_SECONDS = 3
 	still_frame = None
 	start_time = None
+	_overlay = None
 
 	def __init__(self, ctx):
 		TaskFrameProcessorBase.__init__(self, ctx)
@@ -34,10 +38,9 @@ class SnapShotTask(TaskFrameProcessorBase):
 		if self.still_frame is None:
 			stream = io.BytesIO()
 			camera.capture(stream, format='jpeg')
-			data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-			self.still_frame = cv2.imdecode(data, 1)
+			self.still_frame = Image.open(stream)
+			self.__save_image(self.still_frame)
 		if self.start_time is None:
-			self.device_ctx.camera.stop_preview()
 			self.start_time = time.time()
 
 
@@ -45,9 +48,10 @@ class SnapShotTask(TaskFrameProcessorBase):
 		diff_time = int(round(time.time() - self.start_time))
 		print "diff_time {0}".format(diff_time)
 		if diff_time < self.STILL_FRAME_SECONDS:
-			frame = self.still_frame
+			self._overlay = overlay_pil_image_pi(self.device_ctx.camera, self.still_frame, (640, 480))
 		else:
-			self.__save_image(self.still_frame)
+			if self._overlay is not None:
+				self.device_ctx.camera.remove_overlay(self._overlay)
 
 		return frame
 
@@ -56,8 +60,7 @@ class SnapShotTask(TaskFrameProcessorBase):
 
 	def __save_image(self, frame):
 		image_file_name = '/tmp/snapshot{0}.jpg'.format(int(time.time()))
-		
-		sres = cv2.imwrite(image_file_name, frame)
+		im.save(image_file_name, "JPEG")
 		self._is_completed = True
-		self.device_ctx.camera.start_preview()
+
 
