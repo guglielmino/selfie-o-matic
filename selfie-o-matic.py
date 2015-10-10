ae5455322ff8536d5693421206ac618122e391de
Fabrizio Guglielmino <guglielmino@gmail.com>
2015-10-10 08:42:21 +0200

    Added task for recovery images not posted on FB

--- selfie-o-matic.py
+++ selfie-o-matic.py
@@ -2,7 +2,8 @@
 
 # Progetto: Selfie-O-Matic
 
-import sys, os
+import sys
+import os
 import time
 import io
 
@@ -28,19 +29,20 @@ from tasks.task_stillframe import StillFrameTask
 from tasks.task_snapshot import SnapShotTask
 from tasks.task_postonfb import PostOnFbTask
 from tasks.task_pushetta import PushettaTask
+from tasks.task_upload_lost import UploadLostTask
 
 from task_manager import TaskManager
 
 __author__ = "Fabrizio Guglielmino"
 
-APP_NAME="Self-O-Matic"
-VERSION="0.1"
+APP_NAME = "Self-O-Matic"
+VERSION = "0.2"
+
 
 class DeviceContext(object):
     camera = None
     cap = None
     custom_data = {}
-
 
     def __init__(self, camera, cap):
         self.camera = camera
@@ -57,12 +59,11 @@ class SelfieOMatic(object):
     rawCapture = None
     ctx = DeviceContext(None, None)
 
-
     def __init__(self, device=0):
 
         logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                             datefmt='%m-%d %H:%M',
-                            filename='selfie-o-matic.log',level=logging.DEBUG)
+                            filename='selfie-o-matic.log', level=logging.DEBUG)
 
         self.ctx.camera = None
 
@@ -75,20 +76,22 @@ class SelfieOMatic(object):
             self.ctx.camera = PiCamera()
             if settings.HFLIP_IMAGE:
                 self.ctx.camera.hflip = True
-            
+
             self.ctx.camera.framerate = 24
             self.ctx.camera.preview_fullscreen = True
             self.ctx.camera.awb_mode = 'sunlight'
             self.ctx.camera.exposure_mode = 'snow'
-                                                                         
+
             self.ctx.camera.start_preview()
             self.rawCapture = PiRGBArray(self.ctx.camera)
             time.sleep(0.3)
-            
+
         except:
             logging.error("picamera init failed")
 
-
+        # Scheduling del task di recupero immagini non uploadate
+        upload_lost = UploadLostTask(self.ctx)
+        self._task_manager.add_scheduled_task(upload_lost)
 
     def run(self):
         self._is_running = True
@@ -97,13 +100,11 @@ class SelfieOMatic(object):
             self.__process_tasks()
             time.sleep(0.05)
 
-
     def cleanup(self):
         if self.ctx.camera:
             self.ctx.camera.stop_preview()
             self.ctx.camera.close()
         cv2.destroyAllWindows()
-
 
     def __process_input(self):
         key = cv2.waitKey(10)
@@ -111,13 +112,13 @@ class SelfieOMatic(object):
         if GPIO:
             input_state = GPIO.input(18)
             if input_state == False:
-                # Remap 
+                # Remap
                 key = ord('s')
 
         if key == ord('q'):
             cv2.destroyAllWindows()
             self._is_running = False
-            
+
         elif key == ord('s'):
             if not self._is_snap:
                 self._is_snap = True
@@ -131,11 +132,11 @@ class SelfieOMatic(object):
 
                 # Async snapshot, image processing ad post on fb
                 still.set_on_completed(self.__after_fade_event)
-                
-                self._task_manager.add_task(countdown)                
+
+                self._task_manager.add_task(countdown)
                 self._task_manager.add_task(fade)
                 self._task_manager.add_task(still)
-                #self._task_manager.add_task(snap)
+                # self._task_manager.add_task(snap)
                 self._task_manager.add_task(postfb)
 
     def __after_fade_event(self, taskmanager):
@@ -145,7 +146,7 @@ class SelfieOMatic(object):
         taskmanager.add_async_task(snap)
 
     def __process_tasks(self):
-        self._is_snap  = self._task_manager.cycle()
+        self._is_snap = self._task_manager.cycle()
 
 if __name__ == '__main__':
 
